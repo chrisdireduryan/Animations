81337508a7d88e8df25c618a8e4ce2476c36c5ec
Chris Direduryan <direduryan@gmail.com>
2020-12-21 17:27:45 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Chris Direduryan on 21.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
