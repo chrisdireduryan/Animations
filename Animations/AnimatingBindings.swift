b410ccdfba31535dcb4273922df458ba2242f553
Chris Direduryan <direduryan@gmail.com>
2020-12-21 20:26:35 +0300

    Animating bindings

---
+++ Animations/AnimatingBindings.swift
@@ -0,0 +1,36 @@
+//
+//  AnimatingBindings.swift
+//  Animations
+//
+//  Created by Chris Direduryan on 21.12.2020.
+//
+
+import SwiftUI
+
+struct AnimatingBindings: View {
+    @State private var animationAmount: CGFloat = 1
+    var body: some View {
+        print(animationAmount)
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+
+struct AnimatingBindings_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatingBindings()
+    }
+}
--- README.md
+++ README.md
@@ -50,3 +50,28 @@ struct ContentView: View {
     }
 }
 ```
+
+## Animating bindings
+```swift
+struct AnimatingBindings: View {
+    @State private var animationAmount: CGFloat = 1
+    var body: some View {
+        print(animationAmount)
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+```
